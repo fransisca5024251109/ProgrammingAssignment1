5779642c63077b554e92cf15211474818e66f79e
fransisca5024251109 <5024251109@student.its.ac.id>
2026-03-30 09:54:22 +0700

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char ** argv) {
+    string nama;
+    cout << "Silahkan inputkan nama Anda: ";
+    getline(cin, nama); 
+    cout << "Hello " << nama << endl;
+
+    return 0;
+}
